5910b77d283718e95084852bb1b18e04bbb44d77
harishaji <h.krishnan@ai-build.com>
2025-12-21 14:03:47 +0530

    save button

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -4,6 +4,11 @@ export default {
   expo: {
     name: "Tamil AI",
     slug: "tamil-ai",
+
+    android: {
+      package: "com.harishajahan.tamilai",
+    },
+
     extra: {
       apiUrl: process.env.EXPO_PUBLIC_API_URL,
     },
