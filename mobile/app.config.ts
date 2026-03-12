43238fe3c343d3edafe6855d08cd97de632d71d4
Demonhari <harishajidasanyt@gmail.com>
2026-03-12 16:50:00 +0530

    changing name and icon

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -1,21 +1,72 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
   expo: {
-    name: "Tamil AI",
+    name: "J AI",
     slug: "tamil-ai",
     version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "mobile",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
 
     extra: {
-      apiUrl: process.env.EXPO_PUBLIC_API_URL,
+      API_BASE:
+        process.env.EXPO_PUBLIC_API_BASE ||
+        process.env.EXPO_PUBLIC_API_URL ||
+        "https://ai-tool-rrau.onrender.com",
+      apiUrl:
+        process.env.EXPO_PUBLIC_API_URL ||
+        process.env.EXPO_PUBLIC_API_BASE ||
+        "https://ai-tool-rrau.onrender.com",
+      router: {},
+      eas: {
+        projectId: "43fd64c5-dbf3-4e80-8057-ecbb15689e27",
+      },
     },
 
     ios: {
+      supportsTablet: true,
       bundleIdentifier: "com.harishajahan.tamilai",
     },
 
     android: {
       package: "com.harishajahan.tamilai",
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png",
+        backgroundColor: "#E6F4FE",
+      },
+    },
+
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000",
+          },
+        },
+      ],
+    ],
+
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
     },
   },
 };
