abb0202176fd5dfb53c1e45094169a6694ed4434
Demonhari <harishajidasanyt@gmail.com>
2026-05-28 23:53:29 +0530

    feat: add opt-in life context personalization

---
+++ mobile/modules/life-context/android/build/generated/source/buildConfig/release/com/harishajahan/jai/lifecontext/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.harishajahan.jai.lifecontext;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.harishajahan.jai.lifecontext";
+  public static final String BUILD_TYPE = "release";
+}
